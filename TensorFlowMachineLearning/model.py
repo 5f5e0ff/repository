344816d19def8cae0fe5f9ea0ceb12a9e99a8f58
5f5e0ff <utti_honobono@yahoo.co.jp>
2017-11-23 22:06:12 +0900

    テスト位置の可変化,seedの固定,testを変更

--- TensorFlowMachineLearning/model.py
+++ TensorFlowMachineLearning/model.py
@@ -8,23 +8,29 @@ class featuresAndAnswers:
         self.answers = answers
 
 class trainingAndTest():
-    def __init__(self, features, answers ,percentage):
-        training_size = int(len(features) * percentage)
-        self.traning = featuresAndAnswers(features[:training_size],answers[:training_size])
-        self.test = featuresAndAnswers(features[training_size:],answers[training_size:])
+    def __init__(self, features, answers, low, high):
+        low_number = int(len(features)*low)
+        high_number = int(len(features)*high)
+        test_features = features[low_number:high_number]
+        test_answers = answers[low_number:high_number]
+        for i in range(low_number, high_number):
+            features=features.drop([i])
+            answers=answers.drop([i])
+        self.traning = featuresAndAnswers(features, answers)
+        self.test = featuresAndAnswers(test_features, test_answers)
         # N225_1 とかいっぱい
         self.feature_type_count = len(features.columns)
         # positive nagativeの２つ
         self.answer_type_count = len(answers.columns)
 
 class Model:
-    def __init__(self, features, answers, layers=[]):
+    def __init__(self, features, answers, layers=[], low=0.8, high=1.0, seeds=1):
         # 80%をトレーニングに使う
-        self.data = trainingAndTest(features, answers, 0.8)
+        self.data = trainingAndTest(features, answers, low, high)
         # placeholderは変数みたいなもん
         self.real_answer = tf.placeholder(tf.float32,  shape=(None, self.data.answer_type_count))
         self.feature = tf.placeholder(tf.float32, shape=(None, self.data.feature_type_count))
-        self.model = self.createTfModel(layers)
+        self.model = self.createTfModel(layers, seeds)
         # 目標値との誤差 reduce_sumで全部足しちゃう
         cost = -tf.reduce_sum(self.real_answer*tf.log(self.model))
         # 最適化のアルゴリズム。アダムは評価が高いらしいほかにも10個位tensorflow api にある
@@ -35,7 +41,7 @@ class Model:
         correct_prediction = tf.equal(tf.argmax(self.model, 1), tf.argmax(self.real_answer, 1))
         self.accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-    def createTfModel(self, layers):
+    def createTfModel(self, layers, seeds):
         hidden_layer = None
         loop_count_max = len(layers)+1
         for loop_count in range(0,loop_count_max):
@@ -54,7 +60,7 @@ class Model:
                 feature = hidden_layer
                 feature_type_count = layers[loop_count-1]
             # truncated_normal Tensorを正規分布かつ標準偏差0.0001の２倍までのランダムな値で初期化する
-            weights = tf.Variable(tf.truncated_normal([feature_type_count, need_answer_count], stddev=0.0001))
+            weights = tf.Variable(tf.truncated_normal([feature_type_count, need_answer_count], stddev=0.0001, seed=seeds))
             # バイアス
             biases = tf.Variable(tf.ones([need_answer_count]))
             # matmulは掛け算feature * weights
@@ -79,19 +85,61 @@ class Model:
                 print( i, self.session.run(self.accuracy,feed_dict))
 
     def test(self):
+        # test_list
+        feed_dict_t = {
+            self.feature: self.data.test.features,
+            self.real_answer: self.data.test.answers
+        }
         predictions = tf.argmax(self.model, 1)
         real_answers = tf.argmax(self.real_answer, 1)
-        count_correct_answer = tf.reduce_sum(
-            # booleanをfloatに。trueが１になるのでしょう。
+        ones_real_answers = tf.ones_like(real_answers)
+        zeros_real_answers = tf.zeros_like(real_answers)
+        ones_predictions = tf.ones_like(predictions)
+        zeros_predictions = tf.zeros_like(predictions)
+        # 答え01, 予想01
+        zeroone_zeroone = tf.reduce_sum(
             tf.cast(
-                # booleanがもどる
-                tf.equal(real_answers, predictions),
+                tf.logical_and(
+                    tf.equal(real_answers, ones_real_answers),
+                    tf.equal(predictions, ones_predictions)
+                ),
                 tf.float32
             )
         )
-        correct_answer_count = self.session.run(
-            count_correct_answer,
-            {self.feature: self.data.test.features,
-             self.real_answer: self.data.test.answers}
+        # 答え01, 予想10
+        zeroone_onezero = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                    tf.equal(real_answers, ones_real_answers),
+                    tf.equal(predictions, zeros_predictions)
+                ),
+                tf.float32
+            )
         )
-        return  correct_answer_count/len(self.data.test.answers)
+        # 答え10, 予想01
+        onezero_zeroone = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                    tf.equal(real_answers, zeros_real_answers),
+                    tf.equal(predictions, ones_predictions)
+                ),
+                tf.float32
+            )
+        )
+        # 答え10, 予想10
+        onezero_onezero = tf.reduce_sum(
+            tf.cast(
+                tf.logical_and(
+                    tf.equal(real_answers, zeros_real_answers),
+                    tf.equal(predictions, zeros_predictions)
+                ),
+                tf.float32
+            )
+        )
+        one_one, one_zero, zero_one, zero_zero = self.session.run(
+            [onezero_onezero, onezero_zeroone, zeroone_onezero, zeroone_zeroone],
+            feed_dict_t
+        )
+        print("Up_Accuracy\t=", one_one/(one_one+one_zero))
+        print("Down_Accuracy\t=", zero_zero/(zero_one+zero_zero))
+        print("Accuracy\t=", (one_one+zero_zero)/(one_one+one_zero+zero_one+zero_zero))
