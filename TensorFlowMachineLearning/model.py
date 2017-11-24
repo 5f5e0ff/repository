25c8826816a806d8803f8d848003bfd3fda16795
5f5e0ff <utti_honobono@yahoo.co.jp>
2017-11-24 10:16:28 +0900

    ブレイクポイント・ドロップアウトの追加,コスト関数・重みの初期化方法の変更

--- TensorFlowMachineLearning/model.py
+++ TensorFlowMachineLearning/model.py
@@ -32,11 +32,14 @@ class Model:
         # placeholderは変数みたいなもん
         self.real_answer = tf.placeholder(tf.float32,  shape=(None, self.data.answer_type_count))
         self.feature = tf.placeholder(tf.float32, shape=(None, self.data.feature_type_count))
+        self.keep_probin = tf.placeholder_with_default(1.0, shape=None)
+        self.keep_probhid = tf.placeholder_with_default(1.0, shape=None)
         self.model = self.createTfModel(layers, seeds)
-        # 目標値との誤差 reduce_meanで平均を取る 1e-10~1.0に正規化することでlog0を回避
-        cost = -tf.reduce_mean(self.real_answer*tf.log(tf.clip_by_value(self.model,1e-10,1.0)))
+        np.random.seed(seeds)
+        # 目標値との誤差 reduce_meanで平均を取る
+        cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.real_answer, logits=self.model))
         # 最適化のアルゴリズム。アダムは評価が高いらしいほかにも10個位tensorflow api にある
-        self.step = cost, tf.train.AdamOptimizer(learning_rate=0.0001).minimize(cost)
+        self.step = cost, tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
         self.session = tf.Session()
         self.session.run(tf.global_variables_initializer())
         # 正答率の算出 いつもおんなじ？
@@ -48,21 +51,26 @@ class Model:
         loop_count_max = len(layers)+1
         for loop_count in range(0,loop_count_max):
             if loop_count is len(layers) :
-                #最後は与えられた答えの数 positive negativeの2になる
+                # 最後は与えられた答えの数 positive negativeの2になる
                 need_answer_count = self.data.answer_type_count
             else :
-                #隠れ層がある場合はその層のニューロンの数？の答えをだすのね
+                # 隠れ層がある場合はその層のニューロンの数？の答えをだすのね
                 need_answer_count = layers[loop_count]
             if loop_count is 0 :
                 # 最初は与えられた特徴なのね
                 feature = self.feature
                 feature_type_count = self.data.feature_type_count
+                # ドロップアウト
+                feature = tf.nn.dropout(feature, self.keep_probin, seed=seeds)
             else :
                 # 隠れ層がある場合は隠れ層が学習する特徴のデータになるんだ！
                 feature = hidden_layer
                 feature_type_count = layers[loop_count-1]
-            # truncated_normal Tensorを正規分布かつ標準偏差0.0001の２倍までのランダムな値で初期化する
-            weights = tf.Variable(tf.truncated_normal([feature_type_count, need_answer_count], stddev=0.0001, seed=seeds))
+                # ドロップアウト
+                feature = tf.nn.dropout(feature, self.keep_probhid, seed=seeds)
+            # truncated_normal Tensorを正規分布かつ標準偏差stddevの２倍までのランダムな値で初期化する
+            stddev = pow(6/(feature_type_count+need_answer_count), 0.5)
+            weights = tf.Variable(tf.truncated_normal([feature_type_count, need_answer_count], stddev=stddev, seed=seeds))
             # バイアス
             biases = tf.Variable(tf.ones([need_answer_count]))
             # matmulは掛け算feature * weights
@@ -75,21 +83,24 @@ class Model:
                 return tf.nn.softmax(logits)
 
     def train(self,count=30000,print_count=10):
-        # train_list
-        feed_dict = {
-            self.feature: self.data.traning.features,
-            self.real_answer: self.data.traning.answers
-        }
-        # test_list
-        feed_dict_t = {
-            self.feature: self.data.test.features,
-            self.real_answer: self.data.test.answers
-        }
         loss_vec = []
         train_acc = []
         test_acc = []
         print("Epoch \t loss")
         for i in range(1, count+1):
+            number = np.random.choice(len(self.data.traning.features),1000)
+            # train_list
+            feed_dict = {
+                self.feature: self.data.traning.features.iloc[number],
+                self.real_answer: self.data.traning.answers.iloc[number],
+                self.keep_probin: 0.8,
+                self.keep_probhid: 0.5
+            }
+            # test_list
+            feed_dict_t = {
+                self.feature: self.data.test.features,
+                self.real_answer: self.data.test.answers
+            }
             # feed_dictからself.stepを評価, コスト関数を計算
             temp_loss = self.session.run(self.step,feed_dict)
             loss_vec.append(temp_loss[0])
@@ -100,8 +111,12 @@ class Model:
                 train_acc.append(temp_acc_train)
                 test_acc.append(temp_acc_test)
             if i % (count/print_count) == 0:
+                temp = int(count/print_count/4)
                 # コスト関数の表示
-                print( i, "\t", loss_vec[-1])
+                print( i, "\t", np.mean(loss_vec[-temp:-1]))
+                # 長期間更新されなかったとき
+                if(np.mean(loss_vec[-temp:-1]) >= np.mean(loss_vec[-temp*2:-temp])):
+                    break
         # Plot loss over time
         plt.plot(loss_vec, 'k-')
         plt.title('Cross Entropy Loss per Generation')
